a3375bfe665ee0832d185ecfe5e3d8b4fc3269b7
Islam <isxmad@gmail.com>
2021-08-06 13:58:39 +0300

    adaptiv etdim

--- dist/js/script.js
+++ dist/js/script.js
@@ -92,4 +92,83 @@ document.addEventListener('DOMContentLoaded', ()=> {
         }
     });
 
+
+
+
+    //popup btn
+
+    const openBtn = document.querySelector('.feedback__btn');
+    const popup = document.querySelector('.popup');
+    const closeBtn = document.querySelector('.popup__close_btn');
+
+
+    function popupToggler() {
+        openBtn.addEventListener('click', ()=> {
+            popup.classList.add('show_o')
+        });
+        closeBtn.addEventListener('click', ()=> {
+            popup.classList.remove('show_o');
+            popup.classList.add('hide_o');
+        })
+    }
+    popupToggler();
+
+
+
+
+
+    //AOS init
+    AOS.init();
+
+
+
+
+
+
+
+    //Counter
+    let show = true;
+    let countbox = ".counter__item";
+
+    function counter() {
+        $(window).on("scroll load resize", function () {
+            if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+            let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+            let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+            let w_height = $(window).height(); // Высота окна браузера
+            let d_height = $(document).height(); // Высота всего документа
+            let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+            if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+                $('.counter_elem').css('opacity', '1');
+                $('.counter_elem').spincrement({
+                    thousandSeparator: "",
+                    duration: 3000
+                });
+                 
+                show = false;
+            }
+        });
+    }
+
+    counter();
+
+
+
+
+
+
+
+    //burger menu toggler
+    const burgerBtn = document.querySelector('.header__nav_burger');
+    const burgerMenu = document.querySelector('.header__nav_burger_menu');
+    const burgerClose = document.querySelector('.burger_close');
+
+    burgerBtn.addEventListener('click', ()=> {
+        burgerMenu.style.transform = 'translateX(0)';
+    });
+    burgerClose.addEventListener('click', ()=> {
+        burgerMenu.style.transform = 'translateX(100%)';
+    });
+    
+
 });
